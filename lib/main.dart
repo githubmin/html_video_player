921f68d63accf01ed9ef481dcafebb8adbe3a9f1
Daniel <deepblue.daniel@gmail.com>
2021-08-12 17:08:05 +0900

    feat: HtmlVideoWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,16 @@
-import 'package:flutter/material.dart';
+import 'dart:html' as html;
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:universal_ui/universal_ui.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,84 +21,158 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+List<String> videoUrls = [
+  'https://ks3-cn-beijing.ksyun.com/videotest/movie/%E5%A4%A9%E6%B0%94%E4%B9%8B%E5%AD%90/%E5%A4%A9%E6%B0%94%E4%B9%8B%E5%AD%90.m3u8',
+  'https://ks3-cn-beijing.ksyun.com/videotest/short_video/202108091457/m3u8/1.m3u8',
+  'https://vod4.buycar5.cn/20210512/mYvB1MkB/index.m3u8',
+  'https://1251316161.vod2.myqcloud.com/007a649dvodcq1251316161/45d870155285890812491498931/24c2SGTVjrcA.mp4',
+  'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+  'https://v2.88zy.site/20210717/w0WsRtgh/index.m3u8',
+];
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  String _currentVideoUrl = videoUrls[0];
+
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    setState(() => _counter++);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 10.0),
+              HtmlVideoWidget(
+                url: _currentVideoUrl,
+                width: 400.0,
+                height: 226.0,
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () => setState(() => _currentVideoUrl = videoUrls[0]), child: Text('video 1')),
+                  ElevatedButton(onPressed: () => setState(() => _currentVideoUrl = videoUrls[1]), child: Text('video 2')),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: () => setState(() => _currentVideoUrl = videoUrls[2]), child: Text('video 3')),
+                  ElevatedButton(onPressed: () => setState(() => _currentVideoUrl = videoUrls[3]), child: Text('video 4')),
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+      );
+}
+
+class HtmlVideoWidget extends StatefulWidget {
+  final String url;
+  final double width;
+  final double height;
+
+  const HtmlVideoWidget({
+    Key? key,
+    required this.url,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  @override
+  _HtmlVideoWidgetState createState() => _HtmlVideoWidgetState();
+}
+
+class _HtmlVideoWidgetState extends State<HtmlVideoWidget> {
+  String createdViewId = 'map_element';
+  Key _upKey = GlobalKey();
+
+  @override
+  void initState() {
+    _registerFrameElement();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        key: _upKey,
+        width: widget.width,
+        height: widget.height,
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: HtmlElementView(viewType: createdViewId),
+        ),
+      );
+
+  void _registerFrameElement() {
+    ui.platformViewRegistry.registerViewFactory(
+      createdViewId,
+      (int viewId) => html.IFrameElement()
+        ..width = widget.width.toString()
+        ..height = widget.height.toString()
+        ..srcdoc = htmlContent
+        ..style.border = 'none',
     );
   }
+
+  @override
+  void didUpdateWidget(HtmlVideoWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.url != widget.url) {
+      _registerFrameElement();
+      _upKey = GlobalKey();
+    }
+  }
+
+  String get htmlContent => '''
+  <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset=utf-8 />
+    <title>Your title</title>
+      <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
+      <script src="https://unpkg.com/video.js/dist/video.js"></script>
+      <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
+    </head>
+    <body>
+      <video id="my_video_1" class="video-js vjs-fluid vjs-default-skin" controls preload="auto"
+      data-setup='{}'>
+        <source src="${widget.url}" type="application/x-mpegURL">
+      </video>
+    <script>
+    var player = videojs('my_video_1');
+    player.play();
+    </script>
+      
+    </body>
+    </html>
+  ''';
 }
